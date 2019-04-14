b779f5c3baf58a40e8eece7eb00afc1e5e55159c
李嘉庆 <fox.li@lijiaqings-MacBook-Pro.local>
2019-04-14 15:28:58 +0800

    update test

--- src/test/ch09/collection/GenericCollectionMethodTest.java
+++ src/test/ch09/collection/GenericCollectionMethodTest.java
@@ -1,7 +1,18 @@
 package ch09.collection;
 
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+
 import static org.testng.Assert.*;
 
 public class GenericCollectionMethodTest {
 
+    @Test
+    public void test_1() {
+        ArrayList arrayList = new ArrayList();
+        arrayList.add("Test");
+        arrayList.add("Good");
+        assertEquals(GenericCollectionMethod.contains(arrayList,"Test"), true);
+    }
 }
